b40fdc98ee94d0d172ec4318ef1241b84fd5f1eb
Yonsm <I@YONSM.NET>
2017-12-30 23:33:23 +0800

    Refine: 要求

--- PagingView/FlexPagingView.h
+++ PagingView/FlexPagingView.h
@@ -1,5 +1,8 @@
 
 #import <UIKit/UIKit.h>
 
+// 要求：
+//   1. 所有 subview 的宽度与 FlexPagingView 宽度一致；
+//   2. 所有 subview 之间必须保证纵向无间隙，且不重叠；
 @interface FlexPagingView : UIScrollView <UIScrollViewDelegate>
 @end
