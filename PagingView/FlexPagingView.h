d33bab66ab5b9c0a5fcf0f95c6c8a4212dd46e40
Yonsm <I@YONSM.NET>
2018-01-02 12:14:28 +0800

    Precondition

--- PagingView/FlexPagingView.h
+++ PagingView/FlexPagingView.h
@@ -2,8 +2,8 @@
 #import <UIKit/UIKit.h>
 
 // Precondition:
-//   1. subviews[n].width == FlexPagingView.width
-//   2. subviews[n].height >= FlexPagingView.height
-//   3. subviews[n].y + subviews[n].height = subviews[n+1].y
+//   1. subviews[n].height >= FlexPagingView.height
+//   2. subviews[0].y == 0
+//   3. subviews[n].y + subviews[n].height == subviews[n+1].y
 @interface FlexPagingView : UIScrollView <UIScrollViewDelegate>
 @end
